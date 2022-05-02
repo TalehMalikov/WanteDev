aba36e29b7e344294356bded4458dafc11373bb0
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2022-05-02 04:00:37 +0400

    Registration(2)

--- WanteDev/Views/Windows/DeveloperRegistrationWindow.xaml.cs
+++ WanteDev/Views/Windows/DeveloperRegistrationWindow.xaml.cs
@@ -27,5 +27,18 @@ namespace WanteDev.Views.Windows
         {
             Visibility = Visibility.Collapsed;
         }
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = passwordTxt.Password;
+            passwordTxt.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+        }
+
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordTxt.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            passwordTxt.Visibility = Visibility.Visible;
+        }
     }
 }
--- WanteDev/Views/Windows/EmployerRegistrationWindow.xaml.cs
+++ WanteDev/Views/Windows/EmployerRegistrationWindow.xaml.cs
@@ -23,5 +23,22 @@ namespace WanteDev.Views.Windows
         {
             InitializeComponent();
         }
+        private void OnCloseClicked(object sender, EventArgs e)
+        {
+            Visibility = Visibility.Collapsed;
+        }
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = passwordTxt.Password;
+            passwordTxt.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+        }
+
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordTxt.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            passwordTxt.Visibility = Visibility.Visible;
+        }
     }
 }
