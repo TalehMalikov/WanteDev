20cc26af8a04c03986fc950a3d65333cee1f4e43
Taleh Melikov <taleh29@outlook.com>
2022-05-05 23:43:22 +0400

    Mapper

--- WanteDev/Mappers/BaseMapper.cs
+++ WanteDev/Mappers/BaseMapper.cs
@@ -1,17 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using WanteDev.Core.Domain.Entities;
 using WanteDev.Models;
 
-namespace WanteDev.Mappers
+namespace E_Service.Mappers
 {
-    public abstract class BaseMapper<TEntity,TModel> where TEntity : BaseEntity where TModel : BaseModel
+    public class BaseMapper<TEntity, TModel> where TEntity : BaseEntity, new() where TModel : BaseModel, new()
     {
-        public abstract TEntity Map(TModel model);
-        public abstract TModel Map(TEntity entity);
+        public TModel Map(TEntity entity)
+        {
+            TModel model = new TModel();
 
+            Type entitytype = typeof(TEntity);
+
+            Type modeltype = typeof(TModel);
+
+            var modelProps = modeltype.GetProperties();
+
+            var entityProps = entitytype.GetProperties();
+
+            foreach (var modelPropInfo in modelProps)
+            {
+                var entityPropInfo = entityProps.FirstOrDefault(x => x.Name == modelPropInfo.Name);
+
+                if (entityPropInfo == null)
+                    continue;
+
+                var propValue = entityPropInfo.GetValue(entity);
+
+                if (entityPropInfo.PropertyType.BaseType == typeof(BaseEntity))
+                {
+                    Type genericType = typeof(BaseMapper<,>).MakeGenericType(entityPropInfo.PropertyType, modelPropInfo.PropertyType);
+                    var baseMapper = Activator.CreateInstance(genericType);
+
+                    var methods = baseMapper.GetType().GetMethods();
+                    var method = methods.FirstOrDefault(x => x.ReturnType.BaseType == typeof(BaseModel));
+
+                    if (method == null)
+                        continue;
+
+
+                    var modelPropValue = method.Invoke(baseMapper, new object[] { propValue });
+
+                    modelPropInfo.SetValue(model, modelPropValue);
+                }
+                else
+                {
+                    modelPropInfo.SetValue(model, propValue);
+                }
+            }
+
+            return model;
+        }
+
+
+        public TEntity Map(TModel model)
+        {
+            TEntity entity = new TEntity();
+
+            Type entitytype = typeof(TEntity);
+
+            Type modeltype = typeof(TModel);
+
+            var modelProps = modeltype.GetProperties();
+
+            var entityProps = entitytype.GetProperties();
+
+            foreach (var entityPropInfo in entityProps)
+            {
+                var modelPropInfo = modelProps.FirstOrDefault(x => x.Name == entityPropInfo.Name);
+
+                if (modelPropInfo == null)
+                    continue;
+
+                var propValue = modelPropInfo.GetValue(model);
+
+                if (modelPropInfo.PropertyType.BaseType == typeof(BaseModel))
+                {
+                    Type genericType = typeof(BaseMapper<,>).MakeGenericType(entityPropInfo.PropertyType, modelPropInfo.PropertyType);
+                    var baseMapper = Activator.CreateInstance(genericType);
+
+                    var methods = baseMapper.GetType().GetMethods();
+                    var method = methods.FirstOrDefault(x => x.ReturnType.BaseType == typeof(BaseEntity));
+
+                    if (method == null)
+                        continue;
+
+                    var entityPropValue = method.Invoke(baseMapper, new object[] { propValue });
+
+                    entityPropInfo.SetValue(entity, entityPropValue);
+
+                }
+                else
+                {
+                    entityPropInfo.SetValue(entity, propValue);
+                }
+            }
+
+            return entity;
+        }
     }
 }
